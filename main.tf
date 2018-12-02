92aee515b4bfa5e078d11ed4a051b19b04a3bb34
Peter G. Marczis <peter.marczis@nordcloud.com>
2018-12-03 00:17:36 +0100

    Changed to use the right module from the registry.

--- main.tf
+++ main.tf
@@ -7,5 +7,16 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = ""
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "1.46.0"
+
+  name = "vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = false
 }
